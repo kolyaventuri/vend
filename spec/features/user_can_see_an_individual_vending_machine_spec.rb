fcb764915d7db9da6eb8d2d77f67c2746ff8baca
Kolya Venturi <kolya.venturi@enx3s.com>
2018-04-06 10:26:22 -0600

    Snacks spec

--- spec/features/user_can_see_an_individual_vending_machine_spec.rb
+++ spec/features/user_can_see_an_individual_vending_machine_spec.rb
@@ -1,12 +1,45 @@
 require 'rails_helper'
 
 feature 'When a user visits a vending machine show page' do
+  before(:each) do
+    DatabaseCleaner.clean
+  end
+
+  after(:each) do
+    DatabaseCleaner.clean
+  end
   scenario 'they see the location of that machine' do
-    owner = Owner.create(name: "Sam's Snacks")
-    dons  = owner.machines.create(location: "Don's Mixed Drinks")
+    owner = Owner.create!(name: "Sam's Snacks")
+    dons  = owner.machines.create!(location: "Don's Mixed Drinks")
 
     visit machine_path(dons)
 
     expect(page).to have_content("Don's Mixed Drinks Vending Machine")
+  end
+
+  scenario 'they see a list of all the snacks and their prices' do
+    owner = Owner.create!(name: "Sam's Snacks")
+    dons  = owner.machines.create!(location: "Don's Mixed Drinks")
+
+    snacks = [
+      dons.snacks.create!(name: 'Yummy Gummies', price: 200),
+      dons.snacks.create!(name: 'Yucky Uckys', price: 50),
+      dons.snacks.create!(name: 'Hey, not bad.', price: 300)
+    ]
+
+    visit machine_path(dons)
+
+    within('.snacks') do
+      snacks.each do |snack|
+        expect(page).to have_content(snack.name)
+      end
+
+      all('li').each_with_index do |i, item|
+        within(item) do
+          expect(page).to have_content(snacks[i].name)
+          expect(page).to have_content(snacks[i].price / 100.0)
+        end
+      end
+    end
   end
 end
