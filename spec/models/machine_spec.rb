bac93f8f4ccb0c8e6a1cf39bb572833fd3c50733
Kolya Venturi <kolya.venturi@enx3s.com>
2018-04-06 10:40:48 -0600

    Average price test

--- spec/models/machine_spec.rb
+++ spec/models/machine_spec.rb
@@ -1,8 +1,39 @@
 require 'rails_helper'
 
 describe Machine, type: :model do
-  it { is_expected.to belong_to(:owner) }
-  it { is_expected.to have_many(:snacks) }
+  describe 'relationships' do
+    it { is_expected.to belong_to(:owner) }
+    it { is_expected.to have_many(:snacks) }
+  end
 
-  it { is_expected.to validate_presence_of(:location) }
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:location) }
+  end
+
+  describe 'methods' do
+    before(:all) do
+      DatabaseCleaner.clean
+    end
+
+    after(:all) do
+      DatabaseCleaner.clean
+    end
+
+    it 'can calculate average price' do
+      owner = Owner.create!(name: "Sam's Snacks")
+      dons = owner.machines.create!(location: "Don's Mixed Drinks")
+
+      snacks = [
+        dons.snacks.create!(name: 'Yummy Gummies', price: 200),
+        dons.snacks.create!(name: 'Yucky Uckys', price: 50),
+        dons.snacks.create!(name: 'Hey, not bad.', price: 300),
+        dons.snacks.create!(name: 'Sub par at best.', price: 50)
+      ]
+
+      avg = snacks.reduce(0) { |s, snack| s + snack.price }
+      avg /= snacks.length.to_f
+
+      expect(dons.average_price).to be(avg)
+    end
+  end
 end
